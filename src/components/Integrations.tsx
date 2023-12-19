1c5273089802fc38392ce5c2a86149429fa02ced
Cadu Grillo <cadugrillo@gmail.com>
2023-12-18 23:28:39 -0500

    added Axios POST Example

--- src/components/Integrations.tsx
+++ src/components/Integrations.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Button from '@mui/material/Button';
+import axios from "axios";
+
+const MyPost = {
+  id: 1,
+    title: 'My first Post',
+    body: 'This is my first post using Axios',
+    userId: 1,
+}
 
 const Integrations = () => {
+
+  const [posts, setPosts] = useState<any[]>([]);
+
+  const handleClick = () => {
+    axios.post(`https://jsonplaceholder.typicode.com/posts`, { MyPost })
+      .then((res) => {
+        setPosts(res.data);
+        console.log(posts);
+        MyPost.id++;
+      });
+  }
+
   return (
-    <h2>Welcome to Integrations</h2>
+    <>
+      <h2>Welcome to Integrations</h2>
+      <Button variant="contained" onClick={handleClick}>Submit</Button>
+    </>
   )
 }
 
